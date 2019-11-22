babc38d81371a05fbe9b9b6d50d241efb96d4faf
mayur-rawte <rawte.mayur@gmail.com>
2019-11-22 11:32:03 +0530

    Init Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Angular Github Actions
+
+Action to provide node and angular environment .
+
+## Includes
+
+* Angular
+* Protractor
+* Chromium for End to End Test
+
+```
+uses: actions/angular-github-actions
+```
---
+++ action.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function execute(command) {
+   const exec = require('child_process').exec
+   exec(command, (err, stdout, stderr) => {
+     process.stdout.write(stdout)
+   })
+ }
+ execute("npm install -g @angular/cli")
+ execute("npm install -g protractor")
